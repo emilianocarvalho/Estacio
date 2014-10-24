2a493b07f80710500a846525a99a43c891f65671
Emiliano Carvalho <emilianocarvalho.it@gmail.com>
2014-10-23 21:05:30 -0300

    Atualizando classes e teste collection

--- src/Collections/Animal.java
+++ src/Collections/Animal.java
@@ -11,22 +11,27 @@ public class Animal {
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public double getPeso() {
 		return peso;
 	}
+
 	public void setPeso(double peso) {
 		this.peso = peso;
 	}
+
 	public int getCodigo() {
 		return codigo;
 	}
+
 	public void setCodigo(int codigo) {
 		this.codigo = codigo;
 	}
-	
+
 	public void pesquisaAnimal(String nome) {
 		System.out.println();
 	}
--- src/Collections/TestaAnimal.java
+++ src/Collections/TestaAnimal.java
@@ -5,40 +5,39 @@ import java.util.*;
 public class TestaAnimal {
 
 	public static void main(String[] args) {
-
+		
 		Collection colecao = new ArrayList();
 		Scanner sc = new Scanner(System.in);
 		while (true) {
-
-			System.out
-					.println("CADASTRO DE ANIMAIS |||||||||||||||||||||||||||");
+			
+			System.out.println("CADASTRO DE ANIMAIS |||||||||||||||||||||||||||");
 			Animal animal = new Animal();
-
+			
 			System.out.println("Nome do animal:");
 			animal.setNome(sc.next());
 
 			System.out.println("Peso do animal:");
 			animal.setPeso(sc.nextDouble());
-
+			
 			System.out.println("Código para o animal:");
 			animal.setCodigo(sc.nextInt());
-
+			
 			colecao.add(animal);
-
+			
 			System.out.println("0 - Sair");
 			int op = sc.nextInt();
-
+			
 			if (op == 0) {
 				break;
 			}
 		}
-
+		
 		Iterator iteColecao = colecao.iterator();
-
+		
 		while (iteColecao.hasNext()) {
-
+			
 			Animal animalListado = (Animal) iteColecao.next();
-
+			
 			System.out.println("Animal||||||||||||||||||");
 			System.out.println("Código: " + animalListado.getCodigo());
 			System.out.println("Nome: " + animalListado.getNome());
